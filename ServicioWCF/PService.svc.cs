a2b7b5d3146acaed6797588df852278897001222
Stalin <stalinalejandro19@outlook.es>
2023-08-07 01:34:56 -0500

    Add Project building service WCF

---
+++ ServicioWCF/PService.svc.cs
@@ -0,0 +1,24 @@
+﻿using ServicioWCF.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServicioWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class PService : Usuario
+    {
+        public IEnumerable<Usuarios> Consultar()
+        {
+            using (DB_IntComexEntities contexto=new DB_IntComexEntities())
+            {
+                return contexto.Usuarios.AsNoTracking().ToList();
+            }
+        }
+    }
+}
